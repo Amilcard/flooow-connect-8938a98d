8934ec2e2f4e7b0fd84756cb5f64a82653fbb88e
HAMOUDI LAID <59658283+Amilcard@users.noreply.github.com>
2025-12-07 12:58:51 +0100

    fix: logo h-16/h-20 (64px/80px)

--- src/components/common/HeaderLogo.tsx
+++ src/components/common/HeaderLogo.tsx
@@ -28,7 +28,7 @@ export const HeaderLogo = ({ className = "", to = "/" }: HeaderLogoProps) => {
       <img
         src={logoFlooow}
         alt="Flooow - Mes activités, mes aides, mes trajets"
-        className="h-14 md:h-16 w-auto min-w-[140px] md:min-w-[180px] object-contain block"
+        className="h-16 md:h-20 w-auto min-w-[160px] md:min-w-[200px] object-contain block"
       />
     </Link>
   );
